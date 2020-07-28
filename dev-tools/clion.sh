674cd9a781d073b6870cca89575daaeda33378f1
Mark <marco.treglia@akka.eu>
2020-07-28 09:26:43 +0200

    Dev-Tools

---
+++ dev-tools/clion.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+mdkir /tmp/clion/ & pushd /tmp/clion
+wget https://download.jetbrains.com/cpp/CLion-2020.1.3.tar.gz
+tar -xvf  CLion-2020.1.3.tar.gz -C  /opt/clion
+
+
+exit 0
--- installer.sh
+++ installer.sh
@@ -2,9 +2,9 @@
 
 source install_lib.sh
 
-version=$(get_json_var $(curret_path)/config.json version)
-script_folder=$(get_json_var $(curret_path)/config.json script_folder)
-log_folder=$(get_json_var $(curret_path)/config.json log_folder)
+version=$(get_json_var $(curret_path)/configs/dev-tools.json version)
+script_folder=$(curret_path)/dev-tools/
+log_folder=$(get_json_var $(curret_path)/configs/dev-tools.json log_folder)
 
 toilet -F metal "Installer v0.1"
 
